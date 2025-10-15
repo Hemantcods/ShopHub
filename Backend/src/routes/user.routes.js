ce82a64b7366e6cab563dd49272846933b370844
Hemantcods <hy564636@gmail.com>
2025-10-15 15:32:50 +0530

    added secured routes for user

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { registerUser, LoginUser} from "../controllers/user.controller.js";
+import { registerUser, LoginUser, logoutUser, changePassword, getCurrentUser, updateUserDetails } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -10,5 +11,25 @@ router.route("/register").post(
 router.route("/login").post(
     LoginUser
 );
-
+// Protected route
+router.route("/logout").post(
+    verifyJWT,
+    logoutUser
+)
+router.route("/change-password").post(
+    verifyJWT,
+    changePassword
+)
+router.route("/current-user").get(
+    verifyJWT,
+    getCurrentUser
+)
+router.route("/update-user").post(
+    verifyJWT,
+    updateUserDetails
+)
+router.route("/refresh-access-token").post(
+    verifyJWT,
+    refreshAccessToken
+)
 export default router;
