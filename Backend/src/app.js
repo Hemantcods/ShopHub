9964301e97d19e3ac6ff769ba8ba4d94513f2d00
rishiyadav11 <photonics54yadav@gmail.com>
2025-10-16 11:29:09 +0530

    change vercel.json

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(cors({
     origin:process.env.FRONTEND_URL
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
